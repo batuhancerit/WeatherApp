0badf0646239eb7bc787e831a6c33d3b50b4cc80
Batu <batuhancerit01@gmail.com>
2025-12-08 00:57:21 +0300

    add location storage control

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,7 +1,28 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { router } from "expo-router";
+import { getLocationData } from "../utils/storage";
 
 const RootLayout = () => {
+  useEffect(() => {
+    const loadLocation = async () => {
+      const storedLocation = await getLocationData();
+
+      if (storedLocation) {
+        router.replace({
+          pathname: "/weather",
+          params: {
+            latitude: storedLocation.latitude,
+            longitude: storedLocation.longitude,
+          },
+        });
+      }
+    };
+
+    loadLocation();
+  }, []);
+
   return (
     <>
       <StatusBar style="auto" />
